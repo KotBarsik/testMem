52744a80985d090dc88cef2070b65f801c6ba988
salavat <stik229@yandex.ru>
2018-04-20 17:13:50 +0300

    mysql

--- news/events_20180420_133934.sql
+++ news/events_20180420_133934.sql
@@ -20,7 +20,7 @@ CREATE TABLE `event_type` (
 CHARACTER SET = utf8
 COLLATE = utf8_general_ci
 ENGINE = InnoDB
-AUTO_INCREMENT = 16;
+AUTO_INCREMENT = 15;
 -- -------------------------------------------------------------
 -- ---------------------------------------------------------
 
@@ -38,14 +38,30 @@ CREATE TABLE `events` (
 CHARACTER SET = utf8
 COLLATE = utf8_general_ci
 ENGINE = InnoDB
-AUTO_INCREMENT = 11;
+AUTO_INCREMENT = 12;
+-- -------------------------------------------------------------
+-- ---------------------------------------------------------
+
+
+-- CREATE TABLE "users" ------------------------------------
+-- CREATE TABLE "users" ----------------------------------------
+CREATE TABLE `users` ( 
+	`id` Int( 255 ) AUTO_INCREMENT NOT NULL,
+	`login` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	`pwd` VarChar( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+	CONSTRAINT `unique_id` UNIQUE( `id` ) )
+CHARACTER SET = utf8
+COLLATE = utf8_general_ci
+ENGINE = InnoDB
+AUTO_INCREMENT = 2;
 -- -------------------------------------------------------------
 -- ---------------------------------------------------------
 
 
 -- Dump data of "event_type" -------------------------------
-INSERT INTO `event_type`(`id`,`name`) VALUES ( '12', '{"en": "Game", "ru": "Игра"}' );
+INSERT INTO `event_type`(`id`,`name`) VALUES ( '12', '{"en": "Game", "ru": "Игра11"}' );
 INSERT INTO `event_type`(`id`,`name`) VALUES ( '13', '{"en": "Game222", "ru": "Игра11"}' );
+INSERT INTO `event_type`(`id`,`name`) VALUES ( '14', '{"en": "test", "ru": "test"}' );
 -- ---------------------------------------------------------
 
 
@@ -53,6 +69,12 @@ INSERT INTO `event_type`(`id`,`name`) VALUES ( '13', '{"en": "Game222", "ru": "�
 INSERT INTO `events`(`id`,`title`,`text`,`start_time`,`stop_time`,`event_type`) VALUES ( '1', 'О, Счастливчик!11199', '<img src="https://imgholder.ru/323x300/9dbf16/fff&text=image"><div>text text text text</div><h1>asd11asd</h1>', '2018-04-23 07:59:55', '2018-04-18 15:30:08', '12' );
 INSERT INTO `events`(`id`,`title`,`text`,`start_time`,`stop_time`,`event_type`) VALUES ( '5', '43524', 'asdsd', '2018-01-01 00:00:00', '2018-12-31 00:00:00', '1' );
 INSERT INTO `events`(`id`,`title`,`text`,`start_time`,`stop_time`,`event_type`) VALUES ( '10', '333333', 'zczcx', '2018-01-02 23:00:00', '2018-01-31 00:00:00', '12' );
+INSERT INTO `events`(`id`,`title`,`text`,`start_time`,`stop_time`,`event_type`) VALUES ( '11', 'test', 'test', '2018-01-01 00:00:00', '2018-12-01 01:00:00', '14' );
+-- ---------------------------------------------------------
+
+
+-- Dump data of "users" ------------------------------------
+INSERT INTO `users`(`id`,`login`,`pwd`) VALUES ( '1', 'stik229@yandex.ru', '202cb962ac59075b964b07152d234b70' );
 -- ---------------------------------------------------------
 
 
