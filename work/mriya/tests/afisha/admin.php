954c119048a37a65ae4e1332f58e839879f8a2bd
salavat <stik229@yandex.ru>
2018-04-20 19:02:42 +0300

    add footer header

--- work/mriya/tests/afisha/admin.php
+++ work/mriya/tests/afisha/admin.php
@@ -99,23 +99,6 @@ class Admin{
 
 $admin = new Admin();
 $admin->load();
-if($_GET['load']){
-?>
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html>
-<head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="./js/jquery.min.js" type="text/javascript" async="" crossorigin=""></script>
-    <script src="./js/admin.js" type="text/javascript" async="" crossorigin=""></script>
-    <link rel="stylesheet" type="text/css" href="./css/admin.css">
-    <link href="http://bootstrap-v4.ru/examples/starter-template/starter-template.css"
-          tppabs="http://bootstrap-v4.ru/examples/starter-template/starter-template.css" rel="stylesheet">
-    <link href="http://bootstrap-v4.ru/dist/css/bootstrap.min.css"
-          tppabs="http://bootstrap-v4.ru/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<?php
-}
 
 if($_SERVER['REQUEST_URI'] == '/work/mriya/tests/afisha/admin.php' || $_SERVER['REQUEST_URI'] == '/work/mriya/tests/afisha/admin.php?'){
     if($_SESSION['userData']) {
@@ -161,15 +144,4 @@ elseif($_POST['create']){
     else{
         $_SESSION['userLoginError'] = 'Неверный логин или пароль';
     }
-}else{
-    $admin->login();
 }
-
-
-if($_GET['load']) {
-    ?>
-    </body>
-    </html>
-    <?php
-}
-?>
---
+++ work/mriya/tests/afisha/layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./js/jquery.min.js" type="text/javascript" async="" crossorigin=""></script>
+    <script src="./js/admin.js" type="text/javascript" async="" crossorigin=""></script>
+    <link rel="stylesheet" type="text/css" href="./css/admin.css">
+    <link href="http://bootstrap-v4.ru/examples/starter-template/starter-template.css"
+          tppabs="http://bootstrap-v4.ru/examples/starter-template/starter-template.css" rel="stylesheet">
+    <link href="http://bootstrap-v4.ru/dist/css/bootstrap.min.css"
+          tppabs="http://bootstrap-v4.ru/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
