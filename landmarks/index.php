13db081588768ed2112b1ad2752ef46b08ce1a3d
salavat <stik229@yandex.ru>
2018-04-24 18:53:54 +0300

    paralax

--- landmarks/index.php
+++ landmarks/index.php
@@ -4,24 +4,3 @@ require_once './render.php';
 $crud = new CRUD();
 $render = new render($crud);
 $render->render('');
-/*
-$input = [
-    'lat' => 53.5303000,
-    'long' => 49.3461000
-];
-
-$output = [
-    'lat' => 53.2000700,
-    'long' => 50.1500000
-];
-
-
-$m = $crud->calculateTheDistance($input,$output);
-$category = $crud->getCategory();
-$categoryType = $crud->getCategoryType();
-
-foreach ($category as $cat){
-    echo '<a><img src="./images/'.$cat['eng_name'].'.jpg" /></a>';
-}
-*/
-exit();
--- landmarks/render.php
+++ landmarks/render.php
@@ -1,9 +1,24 @@
 <?php
 class render
 {
+    protected $category;
+    protected $categoryType;
+
     public function __construct(CRUD $CRUD)
     {
+        $m = $CRUD->calculateTheDistance(
+            [
+                'lat' => 53.5303000,
+                'long' => 49.3461000
+            ],
+            [
+                'lat' => 53.2000700,
+                'long' => 50.1500000
+            ]
+        );
 
+        $this->category = $CRUD->getCategory();
+        $this->categoryType = $CRUD->getCategoryType();
     }
 
     public function render($data){
@@ -13,7 +28,26 @@ class render
     }
 
     public function content(){
-        echo 'asdas';
+        echo '
+        <script>
+            $(function(){
+                $.stellar({
+                    horizontalScrolling: false,
+                    verticalOffset: 40
+                });
+            });
+        </script>
+		<div class="photo summer" data-stellar-background-ratio="0.5" style="background-position: 50% -32.5px;"><span>Summer</span></div>
+		<div class="photo autumn" data-stellar-background-ratio="0.5" style="background-position: 50% 192.5px;"><span>Autumn</span></div>
+		<div class="photo winter" data-stellar-background-ratio="0.5" style="background-position: 50% 417.5px;"><span>Winter</span></div>
+		<div class="photo spring" data-stellar-background-ratio="0.5" style="background-position: 50% 642.5px;"><span>Spring</span></div>
+		<div class="photo summer" data-stellar-background-ratio="0.5" style="background-position: 50% 867.5px;"><span>Summer</span></div>
+		<div class="photo autumn" data-stellar-background-ratio="0.5" style="background-position: 50% 1092.5px;"><span>Autumn</span></div>
+		<div class="photo winter" data-stellar-background-ratio="0.5" style="background-position: 50% 1317.5px;"><span>Winter</span></div>
+';
+        foreach ($this->category as $cat){
+            //echo '<div><img src="./images/'.$cat['eng_name'].'.jpg" /></div>';
+        }
     }
 
     protected function header(){
