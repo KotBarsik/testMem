b570c7345b2b91d8414768bb2c103129bc780292
salavat <stik229@yandex.ru>
2018-05-03 18:45:15 +0300

    maps

--- landmarks/layout/content.php
+++ landmarks/layout/content.php
@@ -9,4 +9,16 @@
 <div id="mainContentType" style="width:100%;"></div>
 <div id="mainContentObjects" style="width:100%;"></div>
 <div id="mainObject" style="width:100%;"></div>
+<div id="maps">
 
+</div>
+<div id="clickMe" style="width: 100px; position: absolute; height: 100px; z-index: 9999; top: 1px; background-color: #fff;">
+1
+</div>
+<script>
+    $('#clickMe').on('click',function () {
+        $.get('./maps.php', function (data) {
+            $('#maps').append(data);
+        });
+    });
+</script>
